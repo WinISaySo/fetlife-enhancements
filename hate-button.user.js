ce7435952c1c80fd427215b6d2f7822c3058564b
Win <emarshall85@gmail.com>
2025-01-22 23:24:37 -0800

    Fixed label on hate button

--- hate-button.user.js
+++ hate-button.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         The Hate Button
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Replace the down arrow with a broken heart and useful text
 // @author       WinISaySo
-// @match        https://fetlife.com/users/19831492/posts/*
+// @match        https://fetlife.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=fetlife.com
 // @grant        none
 // ==/UserScript==
@@ -23,7 +23,10 @@
                 color: transparent;
                 text-shadow: 0 0 0 rgb(var(--colors-gray-600));
             }
-            [title="Downvoted"]:before, [title="Downvote"]:before {
+            [title="Downvoted"]:before {
+                content: "Unhate · 💔";
+            }
+            [title="Downvote"]:before {
                 content: "Hate · 💔";
             }
         `;
