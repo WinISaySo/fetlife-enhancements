b060c323e22b56ee0e834e80babf5d5c77203952
Win <emarshall85@gmail.com>
2025-01-22 21:14:09 -0800

    Added "Disable View Counts"

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FetLife Enhancements
 What it says on the tin. Small userscripts to make the QoL of FetLife better for the user.
+
+# The Scripts
+
+- [Explore & Remember](https://github.com/WinISaySo/fetlife-enhancements/raw/refs/heads/main/explore-and-remember.user.js): Remembers the filters you use in any of the "Explore" Tabs.
+- [Disable View Counts](https://github.com/WinISaySo/fetlife-enhancements/raw/refs/heads/main/display-view.user.js): Remembers the filters you use in any of the "Explore" Tabs.
---
+++ disable-view-counts.user.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         No More View Counts
+// @namespace    http://tampermonkey.net/
+// @version      0.1.3
+// @description  Hide that anxiety-provoking view counts
+// @author       WinISaySo
+// @match        https://fetlife.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const shouldNotIntercept = evt => !evt.canIntercept || evt.hashChange || evt.downloadRequest || evt.formData;
+
+
+    // wait for a given element to exist before executinmg a callback
+    const whenElementExists = (selector) => new Promise(resolve => {
+        const element = document.querySelector(selector);
+        if (document.querySelector(selector)) return resolve(element);
+
+        const observer = new MutationObserver(mutations => {
+            const element = document.querySelector(selector);
+
+            if (element) {
+                observer.disconnect();
+                resolve(element);
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+
+    const removeViewCount = () => {
+        const viewCount = document.querySelector("[data-views]");
+        viewCount.style.display = "none";
+
+        const spacer = viewCount.nextSibling;
+        spacer.style.display = "none";
+    }
+
+    // handle client-side routing (such as when you click on "Comments"
+    // Firefox doesn't support window.navigation
+    window.navigation && window.navigation.addEventListener("navigate", evt => {
+        if (shouldNotIntercept(evt)) return;
+        whenElementExists("[data-views]").then(removeViewCount);
+    });
+
+
+    // handle initial page load
+    window.addEventListener("load", () => {
+        whenElementExists("[data-views]").then(removeViewCount);
+    });
+})();
