57cb2e0318d0de1f6330cd13f02c5e9ece42f58d
Win <emarshall85@gmail.com>
2025-01-24 23:15:35 -0800

    Cleaned up code

--- red-flags.user.js
+++ red-flags.user.js
@@ -11,93 +11,93 @@
 
 (function() {
   'use strict';
+
+  const DEFAULT_FLAGS = [
+    "BBC",
+    "QOS",
+    "QoS",
+    "Queen of Spades",
+    "Race Play",
+    "Race play",
+    "RacePlay",
+    "Raceplay",
+    "bbc",
+    "black slaves",
+    "bwc",
+    "monkey",
+    "nigger",
+    "qos",
+    "queen of spades",
+    "race play",
+    "raceplay",
+  ];
+
+
   // Only match against profile pages, not activity, pictures, etc.
   // Should have worked with @match above, but doesn't.
-  const DEFAULT_FLAGS = [
-          "race play",
-          "raceplay",
-          "Raceplay",
-          "Race Play",
-          "Race play",
-          "RacePlay",
-          "black slaves"
- ];
-
-
   const profilePageRegex = /https:\/\/fetlife.com\/users\/\d+$/;
   if (!window.location.href.match(profilePageRegex)) {
     return;
   }
 
-  const createRedFlag = text => `
-        <mark class="red-flag">${text}</mark>
-    `;
-
+  // add styles for marked red-flags to the page
   const addStyles = () => {
     const styles = document.createElement("style");
     styles.innerHTML = `
             mark.red-flag {
                 font-weight bold;
-                color: black;
-                background-color: red;
+                color: blue;
+                background-color: orange;
             }
 
         `;
     document.querySelector("head").appendChild(styles);
   }
 
-    const logRedFlag = (elements, flag) => {
-      const found = [];
-      elements.forEach(element => {
-          if (element.textContent.includes(flag)) {
-              found.push({ element, flag });
-          }
-      });
-
-      if (found.length) {
-          found.forEach(match => {
-              match.element.innerHTML = match.element.innerHTML.replace(RegExp(match.flag, "g"), createRedFlag(match.flag));
-          });
-      } else {
-          //console.log({ flag, message: "NOT FOUND" });
+  const markRedFlag = (elements, flag) => {
+    elements.forEach(element => {
+      if (element.textContent.includes(flag)) {
+        element.innerHTML = element.innerHTML.replace(
+          RegExp(flag, "g"), 
+          `<mark class="red-flag">${flag}</mark>`
+        );
       }
+    });
   };
 
-  const CONFIG_MENU = `
+  const addConfigButton = () => {
+    // load exisint flags;
+    const flags = JSON.parse(window.localStorage.getItem("w/fl/red_flags")) || DEFAULT_FLAGS;
 
-  `;
+    // creaet a button that can be used to change flag preferences
+    const menuElement = document.querySelector("[href='/inbox']");
+    const configButton = document.createElement("a");
 
-  const addConfigButton = (flags) => {
-      const menuElement = document.querySelector("[href='/inbox']");
+    configButton.className = menuElement.className;
+    configButton.textContent = "🚩";
 
-      const configButton = document.createElement("a");
-      configButton.className = menuElement.className;
-      configButton.textContent = "🚩";
+    // update flags when that button is clicked
+    configButton.addEventListener("click", () => {
+      const response = prompt(
+        "Which red flags would you like me to spot for you? Paste a comma-separated list to use for them",
+        flags.join(",")
+      );
+      const newFlags = response.split(",");
+      window.localStorage.setItem("w/fl/red_flags", JSON.stringify(newFlags));
+      if (JSON.stringify(newFlags) !== JSON.stringify(flags)) {
+        window.location.reload();
+      }
+    });
 
-      configButton.addEventListener("click", () => {
-          const response = prompt(
-              "Which red flags would you like me to spot for you? Paste a comma-separated list to use for them",
-              flags.join(",")
-          );
-          const newFlags = response.split(",");
-          window.localStorage.setItem("w/fl/red_flags", JSON.stringify(newFlags));
-      });
+    // add the new button along side other menu items
+    menuElement.insertAdjacentElement("beforebegin", configButton);
 
-      menuElement.insertAdjacentElement("beforebegin", configButton);
-      return JSON.parse(window.localStorage.getItem("w/fl/red_flags")) || DEFAULT_FLAGS;
-
+    return flags;
   };
 
   window.addEventListener("load", () => {
     addStyles();
-
-    let flags = JSON.parse(window.localStorage.getItem("w/fl/red_flags")) || DEFAULT_FLAGS;
-    flags = addConfigButton(flags);
-    console.log({ flags });
-    for (let flag of flags) {
-        //
-      logRedFlag(document.querySelectorAll("p,a,span"), flag);
-      //document.body.innerHTML = document.body.innerHTML.replace(new RegExp(flag, "g"), createRedFlag(flag));
-    }
+    const flags = addConfigButton();
+    flags.forEach(flag => markRedFlag(document.querySelectorAll("p,a,span"), flag));
   });
 })();
